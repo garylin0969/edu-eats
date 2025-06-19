691a7927a727a1337f6b67027fa07fa6947df472
Gary <garylin0969@gmail.com>
2025-06-20 00:56:03 +0800

    feat: add Combobox component and integrate it into the Home page

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Combobox from '@/components/molecules/Combobox';
 import DatePicker from '@/components/molecules/DatePicker';
 import { Button } from '@/components/ui/button';
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div className="flex h-screen w-screen flex-col items-center justify-center space-y-4">
             <h1 className="text-4xl font-bold">Hello World</h1>
+            <Combobox />
             <DatePicker />
             <Button>Click me</Button>
         </div>

b217e47e0d82ee346ddefa5b61ee1d03f55b97d4
Gary <garylin0969@gmail.com>
2025-06-20 00:51:55 +0800

    feat: implement DatePicker component and integrate it into the Home page

---
+++ src/components/molecules/DatePicker/DatePicker.tsx
@@ -0,0 +1,39 @@
+import { ChevronDownIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+
+interface DatePickerProps {
+    placeholder?: string;
+}
+
+const DatePicker = ({ placeholder = 'Select date' }: DatePickerProps) => {
+    const [open, setOpen] = useState(false);
+    const [date, setDate] = useState<Date | undefined>(undefined);
+    return (
+        <div className="flex flex-col gap-3">
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" id="date" className="w-48 justify-between font-normal">
+                        {date ? date.toLocaleDateString() : placeholder}
+                        <ChevronDownIcon />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        captionLayout="dropdown"
+                        onSelect={(date) => {
+                            setDate(date);
+                            setOpen(false);
+                        }}
+                    />
+                </PopoverContent>
+            </Popover>
+        </div>
+    );
+};
+
+export default DatePicker;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import DatePicker from '@/components/molecules/DatePicker';
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
     return (
-        <div className="flex h-screen w-screen flex-col items-center justify-center">
-            <h1 className="my-4 text-4xl font-bold">Hello World</h1>
+        <div className="flex h-screen w-screen flex-col items-center justify-center space-y-4">
+            <h1 className="text-4xl font-bold">Hello World</h1>
+            <DatePicker />
             <Button>Click me</Button>
         </div>
     );

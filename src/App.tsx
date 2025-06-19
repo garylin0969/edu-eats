5ea9b91060f6c40d9c09dd7a78e4c9996d4df6bc
hunter850 <hunter850711@gmail.com>
2025-06-19 11:04:14 +0800

    chore: add React Query and React Router for state management and routing, create Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('@/components/pages/Home'));
+
+const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
+const THIRTY_SECONDS_IN_MS = 30 * 60 * 1000;
+const MAX_RETRIES = 1;
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: TEN_MINUTES_IN_MS,
+            gcTime: TEN_MINUTES_IN_MS,
+            retry: MAX_RETRIES,
+            refetchInterval: THIRTY_SECONDS_IN_MS,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+        },
+    },
+});
+const router = createBrowserRouter([
+    {
+        path: '/',
+        // element: <DefaultLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />, //標的總攬
+            },
+        ],
+    },
+]);
+
 function App() {
-    return <div className="text-red-500">Hello World</div>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;

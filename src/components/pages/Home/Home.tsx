42926cb441fedc1d7388b36b108efd20b5eb7670
hunter850 <hunter850711@gmail.com>
2025-06-20 00:16:16 +0800

    chore: add shadcn settins & shadcn Buttont; update Home page with new Button

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="flex h-screen w-screen flex-col items-center justify-center">
+            <h1 className="my-4 text-4xl font-bold">Hello World</h1>
+            <Button>Click me</Button>
+        </div>
+    );
 };
 
 export default Home;

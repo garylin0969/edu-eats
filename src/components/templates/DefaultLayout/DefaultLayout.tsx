c6830c9f1b199fe4d12ca77af846b1fa0497f897
Gary Lin <garylin0969@gmail.com>
2025-06-20 11:18:18 +0800

    style: update header text color in DefaultLayout for improved theming consistency

--- src/components/templates/DefaultLayout/DefaultLayout.tsx
+++ src/components/templates/DefaultLayout/DefaultLayout.tsx
@@ -7,7 +7,7 @@ const DefaultLayout = () => {
         <>
             <header className="sticky top-0 right-0 left-0 z-50 flex h-12 items-center justify-between border-b border-purple-200 bg-white/70 backdrop-blur supports-[backdrop-filter]:bg-white/60">
                 <div className="container mx-auto flex items-center justify-between px-5 md:px-0">
-                    <h1 className="text-lg font-semibold text-gray-900 dark:text-white">Edu Eats</h1>
+                    <h1 className="dark:text-foreground text-lg font-semibold">Edu Eats</h1>
                     <div className="flex items-center gap-2">
                         <ThemeSwitch />
                         <HamburgerMenu />

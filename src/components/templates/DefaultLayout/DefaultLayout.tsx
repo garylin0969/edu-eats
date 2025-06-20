4dc43e51fa79be130a5b5722ac7d3fe96a64c4d4
Gary Lin <garylin0969@gmail.com>
2025-06-20 10:47:26 +0800

    style: adjust padding in DefaultLayout header for improved responsiveness and layout consistency

--- src/components/templates/DefaultLayout/DefaultLayout.tsx
+++ src/components/templates/DefaultLayout/DefaultLayout.tsx
@@ -5,7 +5,7 @@ const DefaultLayout = () => {
     return (
         <>
             <header className="sticky top-0 right-0 left-0 z-50 flex h-12 items-center justify-between border-b border-purple-200 bg-white/70 backdrop-blur supports-[backdrop-filter]:bg-white/60 dark:border-transparent dark:bg-gray-900/70 supports-[backdrop-filter]:dark:bg-gray-900/60">
-                <div className="container mx-auto flex items-center justify-between">
+                <div className="container mx-auto flex items-center justify-between px-5 md:px-0">
                     <h1 className="text-lg font-semibold text-gray-900 dark:text-white">Edu Eats</h1>
                     <ThemeSwitch />
                 </div>

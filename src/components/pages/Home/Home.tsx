17ffe5bca9f7b037586d2005f120ced402be621e
Gary <garylin0969@gmail.com>
2025-06-20 21:38:00 +0800

    refactor: update Home component layout for improved form structure and responsiveness, including new placeholders for form fields

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -11,35 +11,35 @@ const Home = () => {
     const { countyOptions } = useCounty(); // 縣市選項
 
     return (
-        <div className="flex min-h-[calc(100vh-48px)] w-full flex-col items-center justify-center space-y-4">
-            <h2 className="text-4xl font-bold">Hello World</h2>
-
-            <Form {...form}>
-                <form className="w-full">
-                    <FormLayout.Group as={FormLayout.Row}>
-                        <FormLayout.Col lg="4">
-                            <Combobox form={form} name="CountyId" placeholder="縣市" options={countyOptions} />
-                        </FormLayout.Col>
-                        <FormLayout.Col lg="4">
-                            <Combobox form={form} name="CountyId" placeholder="區域" options={countyOptions} />
-                        </FormLayout.Col>
-                        <FormLayout.Col lg="4">
-                            <Combobox form={form} name="CountyId" placeholder="學校院所" options={countyOptions} />
-                        </FormLayout.Col>
-                    </FormLayout.Group>
-                    <FormLayout.Group as={FormLayout.Row}>
-                        <FormLayout.Col lg="5">
-                            <Combobox form={form} name="CountyId" placeholder="縣市" options={countyOptions} />
-                        </FormLayout.Col>
-                        <FormLayout.Col lg="5">
-                            <DatePicker form={form} name="date" />
-                        </FormLayout.Col>
-                        <FormLayout.Col lg="2" className="">
-                            <Button className="w-full">查詢</Button>
-                        </FormLayout.Col>
-                    </FormLayout.Group>
-                </form>
-            </Form>
+        <div className="min-h-[calc(100vh-48px)] w-full">
+            <section className="my-5">
+                <Form {...form}>
+                    <form className="w-full">
+                        <FormLayout.Group as={FormLayout.Row}>
+                            <FormLayout.Col xs="6" md="4">
+                                <Combobox form={form} name="CountyId" placeholder="縣市" options={countyOptions} />
+                            </FormLayout.Col>
+                            <FormLayout.Col xs="6" md="4">
+                                <Combobox form={form} name="CountyId" placeholder="區域" options={countyOptions} />
+                            </FormLayout.Col>
+                            <FormLayout.Col xs="12" md="4">
+                                <Combobox form={form} name="CountyId" placeholder="院所類型" options={countyOptions} />
+                            </FormLayout.Col>
+                        </FormLayout.Group>
+                        <FormLayout.Group as={FormLayout.Row}>
+                            <FormLayout.Col xs="12" lg="5">
+                                <Combobox form={form} name="CountyId" placeholder="學校名稱" options={countyOptions} />
+                            </FormLayout.Col>
+                            <FormLayout.Col xs="6" lg="5">
+                                <DatePicker form={form} name="date" />
+                            </FormLayout.Col>
+                            <FormLayout.Col xs="6" lg="2">
+                                <Button className="w-full">查詢</Button>
+                            </FormLayout.Col>
+                        </FormLayout.Group>
+                    </form>
+                </Form>
+            </section>
         </div>
     );
 };

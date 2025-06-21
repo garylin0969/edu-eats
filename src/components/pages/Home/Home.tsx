7c5a005a691ffbaccabea95889a86cafe81a014a
Gary <garylin0969@gmail.com>
2025-06-21 19:49:07 +0800

    feat: implement form submission handling in Home component with TypeScript support and default values

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -7,17 +7,42 @@ import { Form } from '@/components/ui/form';
 import { useCounty } from '@/hooks';
 import { formatDate } from '@/utils/date';
 
+interface FormData {
+    CountyId: string;
+    AreaId: string;
+    SchoolType: string;
+    SchoolName: string;
+    date: string;
+}
+
 const formatDateValue = (date: Date) => formatDate(date, 'YYYY-MM-DD');
 
 const Home = () => {
-    const form = useForm();
+    // 表單
+    const form = useForm<FormData>({
+        defaultValues: {
+            CountyId: '',
+            AreaId: '',
+            SchoolType: '',
+            SchoolName: '',
+            date: formatDateValue(new Date()),
+        },
+    });
+    const { handleSubmit } = form;
+
+    // 縣市選項
     const { countyOptions } = useCounty();
+
+    // 提交表單
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    };
 
     return (
         <div className="min-h-[calc(100vh-68px)] w-full">
             <section className="my-5">
                 <Form {...form}>
-                    <form className="w-full">
+                    <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
                         <FormLayout.Group as={FormLayout.Row}>
                             <FormLayout.Col xs="6" md="4">
                                 <Combobox form={form} name="CountyId" placeholder="縣市" options={countyOptions} />
@@ -47,7 +72,9 @@ const Home = () => {
                                 <DatePicker form={form} name="date" placeholder="日期" valueFormat={formatDateValue} />
                             </FormLayout.Col>
                             <FormLayout.Col xs="6" lg="2">
-                                <Button className="w-full">查詢</Button>
+                                <Button className="w-full" type="submit">
+                                    查詢
+                                </Button>
                             </FormLayout.Col>
                         </FormLayout.Group>
                     </form>

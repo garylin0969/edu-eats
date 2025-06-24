c9594f520a2061bfaca079a68942cd59b98de231
Gary Lin <garylin0969@gmail.com>
2025-06-24 11:27:59 +0800

    feat: add visually hidden class for accessibility and implement Carousel with Card components in Home section

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -5,7 +5,10 @@ import Combobox from '@/components/molecules/Combobox';
 import DatePicker from '@/components/molecules/DatePicker';
 import FormLayout from '@/components/molecules/FormLayout';
 import Placeholder from '@/components/molecules/Placeholder';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { Form } from '@/components/ui/form';
 import { SCHOOL_TYPE_OPTIONS } from '@/constants';
 import {
@@ -134,6 +137,35 @@ const Home = () => {
                         </FormLayout.Group>
                     </form>
                 </Form>
+            </section>
+            <section>
+                <Carousel className="mx-auto max-w-2xl">
+                    <CarouselContent>
+                        {Array.from({ length: 6 }).map((_, index) => {
+                            return (
+                                <CarouselItem key={index} className="basis-1/3 md:basis-1/4">
+                                    <Card className="overflow-hidden p-0">
+                                        <CardHeader className="visuallyhidden">
+                                            <CardTitle>Card Title</CardTitle>
+                                            <CardDescription>Card Description</CardDescription>
+                                        </CardHeader>
+                                        <CardContent className="p-0">
+                                            <AspectRatio ratio={1}>
+                                                <img
+                                                    className="h-full w-full object-cover"
+                                                    src="https://img.4gamers.com.tw/news-image/8043dc2a-bcfa-4265-934d-4657d05b67a3.jpg"
+                                                    alt=""
+                                                />
+                                            </AspectRatio>
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            );
+                        })}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </section>
             <Placeholder type="empty" />
             <Placeholder type="loading" />

1621c11a388d658d4657d00a77e80055ca1c67d1
Gary Lin <garylin0969@gmail.com>
2025-06-26 09:21:05 +0800

    refactor: export GetCateringService function for improved API accessibility

--- src/api/catering-service.ts
+++ src/api/catering-service.ts
@@ -4,8 +4,6 @@ import { createHttpClient } from './axios';
 // 創建專用的 Express Service API
 const CateringServiceApi = createHttpClient(import.meta.env.VITE_EXPRESS_API_BASE_URL);
 
-const GetCateringService = (params: GetCateringServiceParams) => {
+export const GetCateringService = (params: GetCateringServiceParams) => {
     return CateringServiceApi.get('/api/cateringservice/rest/API', { params });
 };
-
-export default GetCateringService;
